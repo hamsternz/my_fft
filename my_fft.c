97e4572d7a67d516164020b7233a10aa60f9833b
Mike Field <hamster@snap.net.nz>
2020-11-22 23:13:21 +1300

    Added optimized DFT for four points

--- my_fft.c
+++ my_fft.c
@@ -5,8 +5,7 @@
 #include <time.h>
 #include <assert.h>
 
-#define FFT_SIZE (64*1024)
-#define DFT_SIZE 16
+#define FFT_SIZE (4*1024)
 
 double sines[FFT_SIZE];
 double cosines[FFT_SIZE];
@@ -42,6 +41,23 @@ static void print_out(double *d_r, double *d_i, int size, char *message) {
    getchar();
 }
 
+static void dft4(double *r_i, double *i_i, double *r_o, double *i_o, int stride) {
+//   int total_size = stride*4;
+   int total_size = FFT_SIZE;
+   // Bin 0
+   r_o[0] = (r_i[0*stride] + r_i[1*stride] + r_i[2*stride] + r_i[3*stride])/total_size;
+   i_o[0] = (i_i[0*stride] + i_i[1*stride] + i_i[2*stride] + i_i[3*stride])/total_size;
+   // Bin 1
+   r_o[1] = (r_i[0*stride] - i_i[1*stride] - r_i[2*stride] + i_i[3*stride])/total_size;
+   i_o[1] = (i_i[0*stride] + r_i[1*stride] - i_i[2*stride] - r_i[3*stride])/total_size;
+   // Bin 2
+   r_o[2] = (r_i[0*stride] - r_i[1*stride] + r_i[2*stride] - r_i[3*stride])/total_size;
+   i_o[2] = (i_i[0*stride] - i_i[1*stride] + i_i[2*stride] - i_i[3*stride])/total_size;
+   // Bin 3
+   r_o[3] = (r_i[0*stride] + i_i[1*stride] - r_i[2*stride] - i_i[3*stride])/total_size;
+   i_o[3] = (i_i[0*stride] - r_i[1*stride] - i_i[2*stride] + r_i[3*stride])/total_size;
+}
+
 static void dft(double *r_i, double *i_i, double *r_o, double *i_o, int size, int stride) {
    for(int bin = 0; bin < size; bin++) {
       int i = 0;
@@ -74,27 +90,27 @@ static int reverse_bits(int i, int max) {
 static void fft_v2(double *r_i, double *i_i, double *r_o, double *i_o, int size) {
     int i, stride, step;
 
-    stride = size/DFT_SIZE;
+    stride = size/4;
     for(i = 0; i < stride ; i++) {
-        int out_offset = reverse_bits(i,stride) * DFT_SIZE;
-        dft(r_i+i, i_i+i,  r_o+out_offset, i_o+out_offset, DFT_SIZE, stride);
+        int out_offset = reverse_bits(i,stride) * 4;
+        dft4(r_i+i, i_i+i,  r_o+out_offset, i_o+out_offset, stride);
     }
 
-    stride = DFT_SIZE*2;
-    step = FFT_SIZE/stride;
-    while(stride <= FFT_SIZE) {
-       for(i = 0; i < FFT_SIZE; i+= stride) {
+    stride = 8;
+    step = size/stride;
+    while(stride <= size) {
+       for(i = 0; i < size; i+= stride) {
           double *real = r_o+i;
           double *imag = i_o+i;
-          size = stride/2;
-          for(int j = 0; j < size; j++) {
+          int half_size = stride/2;
+          for(int j = 0; j < half_size; j++) {
              double c = cosines[j*step], s = sines[j*step];
-             double rotated_r =  real[size] * c - imag[size] * s;
-             double rotated_i =  real[size] * s + imag[size] * c;
-             real[size] = real[0] - rotated_r;
-             imag[size] = imag[0] - rotated_i;
-             real[0]    = real[0] + rotated_r;
-             imag[0]    = imag[0] + rotated_i;
+             double rotated_r =  real[half_size] * c - imag[half_size] * s;
+             double rotated_i =  real[half_size] * s + imag[half_size] * c;
+             real[half_size] = real[0] - rotated_r;
+             imag[half_size] = imag[0] - rotated_i;
+             real[0]         = real[0] + rotated_r;
+             imag[0]         = imag[0] + rotated_i;
              real++;
              imag++;
           }
@@ -107,15 +123,18 @@ static void fft_v2(double *r_i, double *i_i, double *r_o, double *i_o, int size)
 static void fft_v1(double *r_i, double *i_i, double *r_o, double *i_o, int size, int stride) {
     int half_size = size/2;
 
-    if(half_size >  DFT_SIZE && (half_size & 1) == 0) {
-       fft_v1(r_i,        i_i,        r_o,            i_o,           half_size, stride*2);
-       fft_v1(r_i+stride, i_i+stride, r_o+half_size,  i_o+half_size, half_size, stride*2);
-    } else {
+    if((half_size & 1) != 0) {
        dft(r_i,        i_i,        r_o,            i_o,           half_size, stride*2);
        dft(r_i+stride, i_i+stride, r_o+half_size,  i_o+half_size, half_size, stride*2);
+    } else if (half_size==4) {
+       dft4(r_i,        i_i,        r_o,            i_o,           stride*2);
+       dft4(r_i+stride, i_i+stride, r_o+half_size,  i_o+half_size, stride*2);
+    } else {
+       fft_v1(r_i,        i_i,        r_o,            i_o,           half_size, stride*2);
+       fft_v1(r_i+stride, i_i+stride, r_o+half_size,  i_o+half_size, half_size, stride*2);
     }
 
-    int step = FFT_SIZE/size;
+    int step = stride;
     for(int i = 0; i < half_size; i++) {
        double c = cosines[i*step];
        double s =   sines[i*step];
@@ -147,7 +166,7 @@ void ts_sub(struct timespec *r, struct timespec *a, struct timespec *b) {
 void check_error(double *r_ref, double *i_ref, double *r, double *j, int size) {
    // Check for error
    double error = 0.0;
-   for(int j = 0; j < FFT_SIZE; j++) {
+   for(int j = 0; j < size; j++) {
       error += fabs(dout_r_ref[j] - dout_r[j]); 
       error += fabs(dout_i_ref[j] - dout_i[j]); 
    };
@@ -177,6 +196,7 @@ int main(int argc, char *argv[]) {
    printf("Transform of %5i random complex numbers\n", FFT_SIZE);
    printf("=========================================\n");
 
+   // The standar DFT implementation
    clock_gettime(CLOCK_MONOTONIC, &tv_start_dft);  
    dft(din_r,din_i, dout_r_ref, dout_i_ref, FFT_SIZE,1);
    clock_gettime(CLOCK_MONOTONIC, &tv_end_dft);  
@@ -184,6 +204,7 @@ int main(int argc, char *argv[]) {
    ts_sub(&tv_dft,    &tv_end_dft,    &tv_start_dft);
    printf("DFT %u.%09u secs\n",(unsigned)tv_dft.tv_sec, (unsigned)tv_dft.tv_nsec);
 
+   // The recursive FFT implementation
    clock_gettime(CLOCK_MONOTONIC, &tv_start_fft_v1);  
    fft_v1(din_r,din_i, dout_r,     dout_i, FFT_SIZE, 1);
    clock_gettime(CLOCK_MONOTONIC, &tv_end_fft_v1);  
@@ -192,6 +213,7 @@ int main(int argc, char *argv[]) {
    printf("FFT recursive %u.%09u secs\n",(unsigned)tv_fft_v1.tv_sec, (unsigned)tv_fft_v1.tv_nsec);
    check_error(dout_r_ref, dout_i_ref, dout_r,dout_i, FFT_SIZE);
 
+   // The looping FFT implementation
    clock_gettime(CLOCK_MONOTONIC, &tv_start_fft_v2);  
    fft_v2(din_r,din_i, dout_r,     dout_i, FFT_SIZE);
    clock_gettime(CLOCK_MONOTONIC, &tv_end_fft_v2);  
